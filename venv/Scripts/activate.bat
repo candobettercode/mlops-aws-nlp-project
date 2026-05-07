66dae5426e4f1beb55cf63aaf6125ac7d128a048
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2026-05-08 00:38:45 +0530

    first experiment completed

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=E:\6_learning\mlops-aws-nlp-project\venv
+set VIRTUAL_ENV=D:\Learning\mlops-aws-nlp-project\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
